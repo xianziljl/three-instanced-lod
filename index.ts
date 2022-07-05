5e3e8146e088df74ef5e3e022d7d073941954e45
zhuxianyong <zhuxianyong@baidu.com>
2022-07-05 19:50:39 +0800

    增加了点位动态渲染

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { AmbientLight, AxesHelper, BasicDepthPacking, BasicShadowMap, BoxBufferGeometry, DirectionalLight, DirectionalLightHelper, Mesh, MeshLambertMaterial, MeshPhongMaterial, PCFShadowMap, PCFSoftShadowMap, PerspectiveCamera, PlaneBufferGeometry, Scene, sRGBEncoding, VSMShadowMap, WebGLRenderer } from 'three';
+import { AmbientLight, AxesHelper, BasicDepthPacking, BasicShadowMap, BoxBufferGeometry, CameraHelper, DirectionalLight, DirectionalLightHelper, Frustum, Matrix4, Mesh, MeshLambertMaterial, MeshPhongMaterial, PCFShadowMap, PCFSoftShadowMap, PerspectiveCamera, PlaneBufferGeometry, Scene, sRGBEncoding, VSMShadowMap, WebGLRenderer } from 'three';
 import { MapControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { InstancedLOD } from './src/InstancedLOD';
 
 const scene = new Scene();
 
@@ -14,8 +15,12 @@ document.body.appendChild(renderer.domElement);
 
 
 const camera = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100000);
-camera.position.set(2, 2, 2);
+camera.position.set(0, 20, 0);
 camera.lookAt(0, 0, 0);
+const cameraHelper = new CameraHelper(camera);
+scene.add(cameraHelper);
+
+const camera1 = camera.clone();
 
 
 const helper = new AxesHelper(1000);
@@ -46,21 +51,25 @@ scene.add(dirLight);
 
 
 const controls = new MapControls(camera, renderer.domElement);
-const planeGeom = new PlaneBufferGeometry(10000, 10000, 1000, 1000);
+const planeGeom = new PlaneBufferGeometry(1000, 1000, 1000, 1000);
 planeGeom.rotateX(-Math.PI / 2);
-const planeMtl = new MeshPhongMaterial({ color: 0xaaaaaa });
+const planeMtl = new MeshPhongMaterial({ color: 0x222222 });
 const plane = new Mesh(planeGeom, planeMtl);
 plane.receiveShadow = true;
 scene.add(plane);
 
 
-const cubeGeom = new BoxBufferGeometry();
+const cubeGeom = new BoxBufferGeometry(0.2, 1, 0.2);
+cubeGeom.translate(0, 0.5, 0);
 const cube = new Mesh(cubeGeom, new MeshLambertMaterial({ color: 0xffffff }));
 cube.castShadow = true;
 cube.receiveShadow = true;
-cube.position.set(-1, 0.5, 0);
-scene.add(cube);
 
+let currentCamera = camera;
+document.getElementById('toggle')?.addEventListener('click', () => {
+    currentCamera = currentCamera === camera ? camera1 : camera;
+    controls.object = currentCamera;
+})
 
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -68,23 +77,48 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
 });
 
-const loader = new GLTFLoader();
-loader.load("./assets/Flower.glb", gltf => {
-    console.log(gltf, gltf.scene)
-    gltf.scene.children.forEach((item) => {
-        item.castShadow = true;
-        item.receiveShadow = true;
-    })
-    gltf.scene.scale.set(3, 3, 3);
-    scene.add(gltf.scene);
-});
+// const loader = new GLTFLoader();
+// loader.load("./assets/Flower.glb", gltf => {
+//     console.log(gltf, gltf.scene)
+//     gltf.scene.children.forEach((item) => {
+//         item.castShadow = true;
+//         item.receiveShadow = true;
+//         // meshs.push(item as Mesh);
+//     })
+//     gltf.scene.scale.set(3, 3, 3);
+//     scene.add(gltf.scene);
+// });
 
+const positions = planeGeom.getAttribute('position').array;
+
+
+let meshs: Mesh[] = [];
+const instancedLOD = new InstancedLOD({
+    meshs: [cube],
+    positions,
+    maxDistance: 500,
+    maxCount: 1000
+});
+// instancedLOD.bvhHelper.position.y = 0.1;
+scene.add(instancedLOD);
+
+console.log(scene);
+
+const frustum = new Frustum();
+const cameraMatrix = new Matrix4();
+
+let frame = 0;
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
-    renderer.render(scene, camera);
+    renderer.render(scene, currentCamera);
 
-    // cube.rotation.x += 0.01;
+    if (frame % 2 == 0) {
+        cameraMatrix.multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse);
+        frustum.setFromProjectionMatrix(cameraMatrix);
+        instancedLOD.update(camera, frustum);
+    }
+    frame++;
 }
 
 animate();
--- src/InstancedLOD.ts
+++ src/InstancedLOD.ts
@@ -1,21 +1,149 @@
-import { Camera, Mesh, Object3D } from 'three';
-import { MeshBVH } from 'three-mesh-bvh';
+import { BufferAttribute, BufferGeometry, Camera, DynamicDrawUsage, Frustum, InstancedMesh, Matrix4, Mesh, MeshBasicMaterial, Object3D, Sphere, Vector3 } from 'three';
+import { CONTAINED, INTERSECTED, MeshBVH, MeshBVHVisualizer, NOT_INTERSECTED } from 'three-mesh-bvh';
 
-interface Transforms{
-    positions: Float32Array;
-    rotations: Float32Array;
-    scales: Float32Array;
+interface Transforms {
+    positions: ArrayLike<number>;
+    rotations: ArrayLike<number>;
+    scales: ArrayLike<number>;
 }
 
-export class InstancedLOD {
+interface InstancedLODOptions {
+    meshs: Mesh[];
+    positions: ArrayLike<number>;
+    rotations?: ArrayLike<number>;
+    scales?: ArrayLike<number>;
+    maxDistance?: number;
+    maxCount?: number;
+}
+
+export class InstancedLOD extends Object3D {
+
+    public meshs: Mesh[] = [];
+    public positions: ArrayLike<number>;
+    public rotations: ArrayLike<number>;
+    public scales: ArrayLike<number>;
+    public maxDistance: number;
     public maxCount: number;
-    public bvh: MeshBVH;
 
-    constructor(meshs: Mesh[], transforms: Transforms) {
 
+    public bvhGeometry: BufferGeometry;
+
+    public bvhHelper: MeshBVHVisualizer;
+    public bvhMesh: Mesh;
+
+    private cameraPos = new Vector3();
+
+    private sphere = new Sphere();
+    private dummy = new Object3D();
+
+    public instancedMesh: InstancedMesh;
+    
+
+    constructor(options: InstancedLODOptions) {
+        super();
+        this.meshs = options.meshs;
+        this.positions = options.positions;
+        this.rotations = options.rotations ?? new Float32Array(options.positions.length).fill(0);
+        this.scales = options.scales ?? new Float32Array(options.positions.length).fill(1);
+        this.maxDistance = options.maxDistance ?? 100;
+        this.maxCount = options.maxCount ?? 1000;
+
+        this.bvhGeometry = new BufferGeometry();
+        this.bvhGeometry.setAttribute('position', new BufferAttribute(this.positions, 3));
+
+        const indices = [];
+        const l = this.bvhGeometry.attributes.position.count;
+        for (let i = 0; i < l; i++) {
+            indices.push(i, i, i);
+        }
+        this.bvhGeometry.setIndex(indices);
+        const bvh = new MeshBVH(this.bvhGeometry);
+        this.bvhGeometry.boundsTree = bvh;
+
+        const _mesh = options.meshs[0];
+
+        this.instancedMesh = new InstancedMesh(_mesh.geometry, _mesh.material, this.maxCount);
+        this.instancedMesh.instanceMatrix.setUsage(DynamicDrawUsage);
+        this.add(this.instancedMesh);
     }
 
-    public update(camera: Camera) {
+    public update(camera: Camera, frustum: Frustum) {
+        const { boundsTree } = this.bvhGeometry;
+        if (!boundsTree) return;
 
+        const { cameraPos, sphere, dummy, instancedMesh, positions, maxDistance } = this;
+
+        camera.getWorldPosition(cameraPos);
+        sphere.center.copy(cameraPos);
+        sphere.radius = this.maxDistance;
+
+        const tempVec = new Vector3();
+        const indices: Set<number> = new Set();
+        const indexAttr = this.bvhGeometry.index;
+        const posAttr = this.bvhGeometry.attributes.position;
+        const vectors: Set<Vector3> = new Set();
+
+        if (!indexAttr) return;
+
+        let closestDistance = Infinity;
+        
+        boundsTree.shapecast({
+            traverseBoundsOrder(box) {
+                return box.distanceToPoint(cameraPos);
+            },
+            intersectsBounds(box, isLeaf, score, depth, nodeIndex) {
+                if (score && score > closestDistance) {
+                    return NOT_INTERSECTED;
+                }
+
+                const intersects = frustum.intersectsBox(box) && sphere.intersectsBox(box);
+                return intersects ? INTERSECTED : NOT_INTERSECTED;
+            },
+            intersectsTriangle(triangle, triangleIndex, contained, depth) {
+                if (indices.size < instancedMesh.count && frustum.containsPoint(triangle.a)) {
+
+                    // const dist = triangle.a.distanceTo(cameraPos);
+                    // const x = 1 - dist / maxDistance;
+                    // const n = Math.random();
+                    // if (n <= x) {
+                        const va = indexAttr.getX(triangleIndex * 3);
+                        indices.add(va);
+                    // }
+                }
+            },
+        })
+
+        // console.log(vectors);
+
+    
+
+        // console.log(indices.size, instancedMesh.count);
+
+        // if (instancedMesh.count > indices.size) {
+        //     instancedMesh.count = indices.size;
+        // } else {
+        //     instancedMesh.count = this.maxCount;
+        // }
+
+        // console.log(instancedMesh.count)
+
+        let i = 0;
+        for (let index of indices) {
+            if (i > instancedMesh.count - 1) return;
+
+            tempVec.x = positions[index * 3 + 0];
+            tempVec.y = positions[index * 3 + 1];
+            tempVec.z = positions[index * 3 + 2];
+            
+            dummy.position.copy(tempVec);
+
+            dummy.updateMatrix();
+            instancedMesh.setMatrixAt(i, dummy.matrix);
+
+            i++;
+        }
+
+        
+        instancedMesh.instanceMatrix.needsUpdate = true;
     }
 }
